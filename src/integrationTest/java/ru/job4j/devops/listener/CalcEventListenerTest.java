fd0113c066df22eeebe010f992d049d042c2257a
vadimstrya <vadimstrya@yandex.ru>
2025-02-12 11:24:08 +0700

    Оптимизация работы с Testcontainers [#505373]

--- src/integrationTest/java/ru/job4j/devops/listener/CalcEventListenerTest.java
+++ src/integrationTest/java/ru/job4j/devops/listener/CalcEventListenerTest.java
@@ -1,17 +1,11 @@
 package ru.job4j.devops.listener;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.kafka.KafkaContainer;
-import org.testcontainers.utility.DockerImageName;
 import ru.job4j.devops.CalcEventTypes;
+import ru.job4j.devops.config.ContainersConfig;
 import ru.job4j.devops.models.CalcEvent;
 import ru.job4j.devops.models.User;
 import ru.job4j.devops.repository.CalcEventRepository;
@@ -24,15 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
 @SpringBootTest
-class CalcEventListenerTest {
-
-    private static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>(
-            "postgres:16-alpine"
-    );
-
-    private static final KafkaContainer KAFKA = new KafkaContainer(
-            DockerImageName.parse("apache/kafka:3.7.2")
-    );
+class CalcEventListenerTest extends ContainersConfig {
 
     @Autowired
     private CalcEventRepository calcEventRepository;
@@ -42,27 +28,6 @@ class CalcEventListenerTest {
 
     @Autowired
     private UserRepository userRepository;
-
-    @BeforeAll
-    static void beforeAll() {
-        POSTGRES.start();
-        KAFKA.start();
-    }
-
-    @AfterAll
-    static void afterAll() {
-        POSTGRES.stop();
-        KAFKA.stop();
-    }
-
-    @DynamicPropertySource
-    public static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.kafka.consumer.auto-offset-reset", () -> "earliest");
-        registry.add("spring.kafka.bootstrap-servers", KAFKA::getBootstrapServers);
-        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
-        registry.add("spring.datasource.username", POSTGRES::getUsername);
-        registry.add("spring.datasource.password", POSTGRES::getPassword);
-    }
 
     @Test
     void whenSignupNewEvent() {
--- src/integrationTest/java/ru/job4j/devops/service/CalcServiceTest.java
+++ src/integrationTest/java/ru/job4j/devops/service/CalcServiceTest.java
@@ -1,13 +1,9 @@
 package ru.job4j.devops.service;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
+import ru.job4j.devops.config.ContainersConfig;
 import ru.job4j.devops.models.User;
 import ru.job4j.devops.repository.CalcEventRepository;
 import ru.job4j.devops.repository.UserRepository;
@@ -15,9 +11,7 @@ import ru.job4j.devops.repository.UserRepository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-class CalcServiceTest {
-
-    private static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:16-alpine").withReuse(true);
+class CalcServiceTest extends ContainersConfig {
 
     @Autowired
     private CalcEventRepository calcEventRepository;
@@ -27,23 +21,6 @@ class CalcServiceTest {
 
     @Autowired
     private UserRepository userRepository;
-
-    @DynamicPropertySource
-    public static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
-        registry.add("spring.datasource.username", POSTGRES::getUsername);
-        registry.add("spring.datasource.password", POSTGRES::getPassword);
-    }
-
-    @BeforeAll
-    static void beforeAll() {
-        POSTGRES.start();
-    }
-
-    @AfterAll
-    static void afterAll() {
-        POSTGRES.stop();
-    }
 
     @Test
     public void when5Add6ThenResult11() {
