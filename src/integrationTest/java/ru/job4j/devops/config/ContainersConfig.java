55df61c2e09a6f3ad792ce849f13cc403f7c28ed
vadimstrya <vadimstrya@yandex.ru>
2025-08-06 17:24:04 +0700

    Выпилил Кафку

--- src/integrationTest/java/ru/job4j/devops/config/ContainersConfig.java
+++ src/integrationTest/java/ru/job4j/devops/config/ContainersConfig.java
@@ -4,8 +4,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.kafka.KafkaContainer;
-import org.testcontainers.utility.DockerImageName;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
@@ -16,19 +14,12 @@ public abstract class ContainersConfig {
             "postgres:17"
     );
 
-    private static final KafkaContainer KAFKA = new KafkaContainer(
-            DockerImageName.parse("apache/kafka:3.7.2")
-    );
-
     static {
         POSTGRES.start();
-        KAFKA.start();
     }
 
     @DynamicPropertySource
     public static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.kafka.consumer.auto-offset-reset", () -> "earliest");
-        registry.add("spring.kafka.bootstrap-servers", KAFKA::getBootstrapServers);
         registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
         registry.add("spring.datasource.username", POSTGRES::getUsername);
         registry.add("spring.datasource.password", POSTGRES::getPassword);
